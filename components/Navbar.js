cdb890be89aff40786be11e1176ed257c035a5b1
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-04-06 02:22:17 +0530

    Navbar Metamask component completed

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,67 @@
 import React from "react";
-function Navbar (){
+import { useEffect, useState } from 'react';
+
+function Navbar() {
+    const [walletAddress, setwalletAddress] = useState("");
+    useEffect(() => {
+        if (walletAddress) {
+            document.getElementById("button").classList.add("accountActive");
+        }
+        else {
+            console.log(walletAddress)
+            document.getElementById("button").classList.remove("accountActive");
+        }
+        getcurrentconnectedAccounts();
+        addWalletListener();
+    })
+    const connectWallet = async () => {
+        if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+            try {
+                const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+                setwalletAddress(accounts[0]);
+            }
+            catch (e) {
+                console.log(e);
+            }
+        }
+        else {
+            console.log("Please install metamask")
+        }
+    }
+
+    const addWalletListener = async () => {
+        if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+            window.ethereum.on("accountsChanged", (accounts) => {
+                setwalletAddress(accounts[0])
+                console.log(accounts[0])
+            })
+        }
+        else {
+            setwalletAddress("");
+        }
+    }
+
+    const getcurrentconnectedAccounts = async () => {
+        if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+            try {
+                const accounts = await window.ethereum.request({ method: "eth_accounts" });
+                if (accounts[0].length > 0) {
+                    setwalletAddress(accounts[0]);
+                    console.log(accounts[0])
+                }
+            }
+            catch (e) {
+                console.log(e);
+            }
+        }
+        else {
+            console.log("Please install metamask")
+        }
+    }
     return (
         <div>
-            <img src="./Web3Drive.png" width={150} height={100}></img>
+            <img src="./Web3Drive.png" width={150} height={100} className="logo"></img>
+            <button className="connectButton" id="button" onClick={connectWallet} >{(walletAddress && walletAddress.length > 0) ? `Connected to ${walletAddress.substring(0, 4)}...${walletAddress.substring(38, 42)}` : "Connect to Metamask 🦊"}</button>
         </div>
     )
 }
